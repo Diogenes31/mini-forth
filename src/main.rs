33b9abd6da9e9903b2d1a3017079448d61eb6b49
diogenes <diogenes@diogenes.top>
2023-03-07 23:21:29 -0800

    Add a little bit of stack handling
    This is gonna ramp up fast methinks. Can't wait.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use rustyline::error::ReadlineError;
 use rustyline::{DefaultEditor, Result};
 
+use std::collections::VecDeque;
+use std::string::String;
+
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const PROMPT: &str = "4th: ";
 
 fn main() -> Result<()> {
     println!("mini-forth {}", VERSION);
     let mut rl = DefaultEditor::new()?;
+    let mut stack: VecDeque<String> = VecDeque::new();
 
     if rl
         // This is a hack, obviously a history file should go somewhere better than this.
@@ -22,7 +26,20 @@ fn main() -> Result<()> {
             Ok(line) => {
                 rl.add_history_entry(line.as_str())
                     .expect("Error adding to history");
-                println!("Line: {}", line);
+                print!("[");
+
+                let stack_iter = stack.iter();
+                for value in stack_iter {
+                    print!("\"{}\", ", value);
+                }
+
+                stack.push_back(String::from(line.as_str()));
+                println!(
+                    "\"{}\"]",
+                    stack
+                        .back()
+                        .expect("Something has gone terribly wrong during stack construction")
+                );
             }
             Err(ReadlineError::Interrupted) => {
                 println!("ctrl+c");
